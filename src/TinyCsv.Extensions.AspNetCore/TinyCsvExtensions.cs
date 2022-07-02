0b828f50f8d7865c10d3cf8cae860fa215a752b8
Federico Mazzanti <f.mazzanti@adiacent.com>
2022-07-02 19:16:04 +0200

    create aspnetcore extension

---
+++ src/TinyCsv.Extensions.AspNetCore/TinyCsvExtensions.cs
@@ -0,0 +1,81 @@
+﻿/// <summary>
+/// 
+/// The MIT License (MIT)
+/// 
+/// Copyright (c) 2022 Federico Mazzanti
+/// 
+/// Permission is hereby granted, free of charge, to any person
+/// obtaining a copy of this software and associated documentation
+/// files (the "Software"), to deal in the Software without
+/// restriction, including without limitation the rights to use,
+/// copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following
+/// conditions:
+/// 
+/// The above copyright notice and this permission notice shall be
+/// included in all copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+/// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+/// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+/// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+/// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+/// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+/// OTHER DEALINGS IN THE SOFTWARE.
+/// 
+/// </summary>
+
+namespace TinyCsv.Extensions.AspNetCore
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection.Extensions;
+    using System;
+
+    /// <summary>
+    /// TinyCsv Extensions
+    /// </summary>
+    public static class TinyCsvExtensions
+    {
+        /// <summary>
+        /// Add TinyCsv to service for specific service name
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="serviceName"></param>
+        /// <param name="options"></param>
+        public static void AddTinyCsv<T>(this IServiceCollection services, string serviceName, Action<CsvOptions<T>> options)
+            where T : class, new()
+        {
+            var tinyCsvFactory = services.GetTinyCsvFactory();
+            var tinyCsv = new TinyCsv<T>(options);
+            tinyCsvFactory.Add(serviceName, tinyCsv);
+        }
+
+        /// <summary>
+        /// Add TinyCsv to service
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="serviceName"></param>
+        /// <param name="options"></param>
+        public static void AddTinyCsv(this IServiceCollection services)
+        {
+            services.TryAddSingleton<ITinyCsvFactory, TinyCsvFactory>();
+        }
+
+        /// <summary>
+        /// Get TinyCsvFactory
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        static ITinyCsvFactory GetTinyCsvFactory(this IServiceCollection services)
+        {
+            services.AddTinyCsv();
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+            var tinyCsvFactory = serviceProvider.GetService<ITinyCsvFactory>();
+            return tinyCsvFactory;
+        }
+    }
+}
---
+++ src/TinyCsv.Extensions.AspNetCore/TinyCsvFactory.cs
@@ -0,0 +1,94 @@
+﻿/// <summary>
+/// 
+/// The MIT License (MIT)
+/// 
+/// Copyright (c) 2022 Federico Mazzanti
+/// 
+/// Permission is hereby granted, free of charge, to any person
+/// obtaining a copy of this software and associated documentation
+/// files (the "Software"), to deal in the Software without
+/// restriction, including without limitation the rights to use,
+/// copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following
+/// conditions:
+/// 
+/// The above copyright notice and this permission notice shall be
+/// included in all copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+/// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+/// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+/// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+/// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+/// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+/// OTHER DEALINGS IN THE SOFTWARE.
+/// 
+/// </summary>
+
+namespace TinyCsv.Extensions.AspNetCore
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// TinyCsv Factory
+    /// </summary>
+    public sealed class TinyCsvFactory : ITinyCsvFactory
+    {
+        private static Dictionary<string, object> _tinyCsv = new Dictionary<string, object>();
+
+        /// <summary>
+        /// TinyCsv Factory
+        /// </summary>
+        public TinyCsvFactory()
+        {
+
+        }
+
+        /// <summary>
+        /// Add TinyCsv with service's name
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="serviceName"></param>
+        /// <param name="tinyCsv"></param>
+        /// <exception cref="Exception"></exception>
+        public void Add<T>(string serviceName, ITinyCsv<T> tinyCsv) where T : class, new()
+        {
+            if (_tinyCsv.ContainsKey(serviceName))
+            {
+                throw new Exception($"TinyCsv with name {serviceName} already exists");
+            }
+            _tinyCsv.Add(serviceName, tinyCsv);
+        }
+
+        /// <summary>
+        /// Create new TinyCsv
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public ITinyCsv<T> Create<T>(Action<CsvOptions<T>> options) where T : class, new()
+        {
+            ITinyCsv<T> tinyCsv = new TinyCsv<T>(options);
+            return tinyCsv;
+        }
+
+        /// <summary>
+        /// Get TinyCsv by service's name
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="serviceName"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public ITinyCsv<T> Get<T>(string serviceName) where T : class, new()
+        {
+            if (!_tinyCsv.ContainsKey(serviceName))
+            {
+                throw new Exception($"TinyCsv with name {serviceName} does not exist");
+            }
+            return (ITinyCsv<T>)_tinyCsv[serviceName];
+        }
+    }
+}
