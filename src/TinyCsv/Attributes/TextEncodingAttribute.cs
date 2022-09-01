8f448f001ba9dc51a8f43fa8b27e73ac07014c99
Federico Mazzanti <f.mazzanti@adiacent.com>
2022-09-01 17:03:28 +0200

    adding class attributes

---
+++ src/TinyCsv/Attributes/TextEncodingAttribute.cs
@@ -0,0 +1,57 @@
+﻿/// <summary>
+/// 
+/// The MIT License (MIT)
+/// 
+/// Copyright (c) 2022 Federico Mazzanti
+/// 
+/// Permission is hereby granted, free of charge, to any person
+/// obtaining a copy of this software and associated documentation
+/// files (the "Software"), to deal in the Software without
+/// restriction, including without limitation the rights to use,
+/// copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following
+/// conditions:
+/// 
+/// The above copyright notice and this permission notice shall be
+/// included in all copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+/// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+/// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+/// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+/// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+/// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+/// OTHER DEALINGS IN THE SOFTWARE.
+/// 
+/// </summary>
+
+namespace TinyCsv.Attributes
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// Represents a character encoding
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public sealed class TextEncodingAttribute : Attribute
+    {
+        /// <summary>
+        /// Represents a character encoding
+        /// </summary>
+        public Encoding TextEncoding { get; private set; }
+
+        /// <summary>
+        /// Contructor
+        /// </summary>
+        /// <param name="hasHeaderRecord"></param>
+        public TextEncodingAttribute(Encoding encoding = null)
+            : base()
+        {
+            TextEncoding = encoding ?? Encoding.UTF8;
+        }
+    }
+
+}
