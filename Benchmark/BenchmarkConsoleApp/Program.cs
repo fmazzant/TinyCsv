d555f2c135d1ffdee51d0080f550afaa29deafd8
Federico Mazzanti <f.mazzanti@adiacent.com>
2022-10-18 01:02:46 +0200

    create benchmark project

---
+++ Benchmark/BenchmarkConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Text;
+
+namespace BenchmarkConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MemoryBenchmarkerDemo>();
+        }
+    }
+
+
+    [MemoryDiagnoser]
+    public class MemoryBenchmarkerDemo
+    {
+        int NumberOfItems = 100000;
+
+        [Benchmark]
+        public string ConcatStringsUsingStringBuilder()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < NumberOfItems; i++)
+            {
+                sb.Append("Hello World!" + i);
+            }
+            return sb.ToString();
+        }
+        [Benchmark]
+        public string ConcatStringsUsingGenericList()
+        {
+            var list = new List<string>(NumberOfItems);
+            for (int i = 0; i < NumberOfItems; i++)
+            {
+                list.Add("Hello World!" + i);
+            }
+            return list.ToString();
+        }
+    }
+}
